779bbc4d7e852e19b310983bad2a065afd9278a0
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2024-06-05 13:37:57 -0500

    Changes

--- E_Registro_Notas.py
+++ E_Registro_Notas.py
@@ -1,10 +1,76 @@
 from E_Datos_Notas import*
 from C_Registro_Estudiantes import*
-# def Modulos_Elegir ():
+from F_Datos_Rutas import*
+from F_Registro_Rutas import*
+def Modulos_Elegir ():
+    try: 
+        print(" ----------------------------")
+        modulos = ["1. Para Fundamentos de programacion", 
+                   "2. Para Programacion Web ",  "3. Para Programación formal",
+                    "4. Para Bases de datos" , " 5. Para Backend"]
+        for i in modulos:       
+            print(i)
+        op = int(input("ingrese la opcion deseada ="))   
+        if op == 1:
+            modulo = "Para Programacion Web" 
+        elif op == 2:
+            modulo = "Fundamentos de programacion"
+        elif op == 3:
+            modulo = " Programación formal"
+        elif op == 4:
+            modulo = "Para Bases de datos"
+        elif op == 5:
+            modulo = "Para Backend"
+        else:
+            print("No valido")
+            return
+        return  modulo
+                                
+    except Exception as e:
+                print(f"Se ha producido un error: {e}")
+                print("Por favor, intente nuevamente.")
+########################################################################
+def Registro_moduloX():
+    rutas = cargar_datos_rutas()
+    
+    print("-------------------------------------------------")
+    print("Una vez culminado el modulo agregue todas las notas")
+    rutas = cargar_datos_rutas()
+    doc = input("Ingrese numero de documento del estudiante:  ")
+    hora = salon_Hotario ()
+    Nombre_Grupo = input("Ingrese grupo, ej. U2, U1, A2: ")
+    modulo = Modulos_Elegir ()
+    
+        #ruta[hora][Nombre_Grupo]["Aula"][doc][modulo] = {""}
+    notas_modulo = {}
+    notas_modulo["QuiZ#1"] = int(input("Ingrese la Nota del Quiz#1:  "))
+    notas_modulo["QuiZ#2"] = int(input("Ingrese la Nota del Quiz#2:  "))
+    notas_modulo["Tarea#1"] = int(input("Ingrese la Nota delTarea#1:  "))
+    notas_modulo["Tarea#2"] = int(input("Ingrese la Nota del Tarea#2:  "))
+    notas_modulo["Proyecto"] = int(input("Ingrese la Nota delProyecto:  "))
+    notas_modulo["Evaluacion"] =int(input("Ingrese la Nota del Evaluacion:  "))
+    for ruta in rutas:
+        if hora in ruta:
+            if Nombre_Grupo in ruta[hora]:
+                if "Aula" in ruta[hora][Nombre_Grupo]:
+                    if doc in ruta[hora][Nombre_Grupo]["Aula"]:
+                        if modulo not in ruta[hora][Nombre_Grupo]["Aula"][doc]:
+                            ruta[hora][Nombre_Grupo]["Aula"][doc][modulo] ={}
+                        ruta[hora][Nombre_Grupo]["Aula"][doc][modulo] = notas_modulo
+                        
+            
+            
+    
+        
+    guardar_datos_notas(rutas)
+        
+                
+Registro_moduloX ()
 
 
 
 
+        
 
 def Registro_modulo_Fundamentos_de_programacioon():
     while True:
